a19c93ef6946321fd86456e3a1b7ad2eecc990a2
wilbertharianto8 <84430781+wilbertharianto8@users.noreply.github.com>
2022-06-18 03:38:35 +0700

    Initial commit

---
+++ build/precache-manifest.f77b7513dd874bf71296f5fd2a6cc573.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dfa8bb8336afa1950f430b75466bb2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0a176ea7e10879237de",
+    "url": "/static/css/2.1cace5d8.chunk.css"
+  },
+  {
+    "revision": "637566c40684eaa533b0",
+    "url": "/static/css/main.e2b5107d.chunk.css"
+  },
+  {
+    "revision": "f0a176ea7e10879237de",
+    "url": "/static/js/2.cdd3e860.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cdd3e860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "637566c40684eaa533b0",
+    "url": "/static/js/main.800ec5d7.chunk.js"
+  },
+  {
+    "revision": "d0c8a637ec613b149876",
+    "url": "/static/js/runtime-main.0e71fcbe.js"
+  }
+]);
